2a694f0ffb2795f47ed663f492c29739fd38bbb8
Cesar Leonardo <cmoralesleonardo@gmail.com>
2023-01-11 16:55:00 -0300

    Atualizando os Exercicios

---
+++ exerciciosJava/src/lambdas/desafio/DesafioBinaryOperator.java
@@ -0,0 +1,63 @@
+package lambdas.desafio;
+
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+import lambdas.Produto;
+
+public class DesafioBinaryOperator {
+
+	public static void main(String[] args) {
+		Produto iPad = new Produto("IPad", 4235.89, 0.13);
+		Produto iPhone = new Produto("IPhone", 2300.00, 0.05);
+		
+		Function<Produto, Double> valorReal = 
+				prod -> (prod.preco * (1 - prod.desconto));
+				
+		System.out.printf("------------------------------------------- \n"
+				+ "Valor com Desconto dos Produtos: \n\n"
+				+ "%s: %.2f \n"
+				+ "%s: %.2f \n"
+				+ "------------------------------------------- \n", 
+				iPad.nome, valorReal.apply(iPad), 
+				iPhone.nome, valorReal.apply(iPhone));
+		
+		
+		
+		UnaryOperator<Double> impostoMunicipal = 
+				imposto -> (imposto >= 2500) ? (imposto + (imposto * 0.085)) : imposto;
+				
+				System.out.printf("------------------------------------------- \n"
+						+ "Valor do Produto com o Imposto Municipal: \n\n"
+						+ "%s: %.2f \n"
+						+ "%s: %.2f \n"
+						+ "------------------------------------------- \n", 
+						iPad.nome, valorReal.andThen(impostoMunicipal).apply(iPad), 
+						iPhone.nome, valorReal.andThen(impostoMunicipal).apply(iPhone));
+				
+		
+				
+		UnaryOperator<Double> freteEntrega = 
+				frete -> (frete >= 3000) ? frete + 100 : frete + 50;
+				
+				System.out.printf("------------------------------------------- \n"
+						+ "Valor do Produto com o Frete: \n\n"
+						+ "%s: %.2f \n"
+						+ "%s: %.2f \n"
+						+ "------------------------------------------- \n", 
+						iPad.nome, valorReal.andThen(impostoMunicipal).andThen(freteEntrega).apply(iPad), 
+						iPhone.nome, valorReal.andThen(impostoMunicipal).andThen(freteEntrega).apply(iPhone));
+	
+				
+				
+		System.out.printf("------------------------------------------- \n"
+						+ "Valor Final do Produto Formatado: \n\n"
+						+ "%s: R$ %.2f \n"
+						+ "%s: R$ %.2f \n"
+						+ "------------------------------------------- \n", 
+						iPad.nome, valorReal.andThen(impostoMunicipal).andThen(freteEntrega).apply(iPad), 
+						iPhone.nome, valorReal.andThen(impostoMunicipal).andThen(freteEntrega).apply(iPhone));
+	}
+}
