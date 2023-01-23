f03387388f96f5ee3ae012a37538fcd36b207c1a
Cesar Leonardo <cmoralesleonardo@gmail.com>
2023-01-23 17:16:34 -0300

    Atualizando os exercicios

--- exerciciosJava/src/streams/Reduce3.java
+++ exerciciosJava/src/streams/Reduce3.java
@@ -2,14 +2,14 @@ package streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public class Reduce2 {
+public class Reduce3 {
 
 	public static void main(String[] args) {
-		
 		Aluno a1 = new Aluno("Ana", 7.1);
 		Aluno a2 = new Aluno("Luna", 6.1);
 		Aluno a3 = new Aluno("Gabi", 8.1);
@@ -19,12 +19,18 @@ public class Reduce2 {
 		
 		Predicate<Aluno> aprovado = a -> a.nota >= 7;
 		Function<Aluno, Double> apenasNota = a -> a.nota;
-		BinaryOperator<Double> somatorio = (a, b) -> a + b;
 		
-		alunos.stream()
+		BiFunction<Media, Double, Media> calcularMedia =
+				(media, nota) -> media.adicionar(nota);
+				
+		BinaryOperator<Media> combinarMedia=
+				(m1, m2) -> Media.combinar(m1, m2);
+		
+		Media media = alunos.stream()
 			.filter(aprovado)
 			.map(apenasNota)
-			.reduce(somatorio)
-			.ifPresent(System.out::println);
+			.reduce(new Media(), calcularMedia, combinarMedia);
+		
+		System.out.println("A media dos aprovados eh " + media.getValor());
 	}
 }
